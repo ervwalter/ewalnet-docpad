e14250808653b5d1fc80015e237fe729028c0806
Erv Walter <erv@ewal.net>
2014-12-16 19:34:45 -0600

    add web api

---
+++ webapi/GamesWebAPI/GamesControllers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GamesWebAPI
+{
+    public class CollectionController : ApiController
+    {
+        // GET api/<controller>
+        public async Task<List<CollectionItem>> Get()
+        {
+            var collection = await CacheManager.GetOrCreateObjectAsync("edwalter", false, 15, async (k) => {
+                var provider = new BggDataProvider();
+                return await provider.GetCollection("edwalter");
+            });
+            return collection.Games;
+        }
+    }
+
+    public class PlaysController : ApiController
+    {
+        // GET api/<controller>
+        public async Task<List<PlayItem>> Get()
+        {
+            var plays = await CacheManager.GetOrCreateObjectAsync("edwalter", false, 15, async (k) =>
+            {
+                var provider = new BggDataProvider();
+                return await provider.GetPlays("edwalter");
+            });
+            return plays.Items;
+        }
+
+    }
+
+}
---
+++ webapi/GamesWebAPI/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace GamesWebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.Remove(config.Formatters.FormUrlEncodedFormatter);
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+            config.Formatters.JsonFormatter.Indent = true;
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
